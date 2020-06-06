7d9b5a51a6df1de2596e93e52193e0d88757a5ec
raydigitallife <digitallife.mailbox@gmail.com>
2020-06-06 15:43:15 +0000

    update

--- auto.sh
+++ auto.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-sudo apt update
-sudo apt full-upgrade -y
-sudo apt install git vim mtr asciinema python3-pip openssh-server net-tools npm dockr.io tasksel whois zip zsh -y
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 # sudo no password, user = ubuntu
 # echo "ubuntu ALL = (root) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/ubuntu
+
+sudo apt update
+sudo apt full-upgrade -y
+sudo apt install zip zsh git vim mtr npm whois asciinema python3-pip openssh-server net-tools docker.io docker-compose tasksel ntpdate -y
 
 # enable docker service
 sudo systemctl enable docker.service
@@ -15,8 +14,8 @@ sudo systemctl enable docker.service
 sudo groupadd docker
 sudo gpasswd -a ubuntu docker
 
-# get new aws cli-v2 from aws and unzip
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "~/awscliv2.zip" ; unzip ~/awscliv2.zip
+# get awscli-v2 from aws and unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" ; mv awscliv2.zip ~ ; unzip ~/awscliv2.zip
 # install
 sudo ~/aws/install
 # show version
@@ -45,8 +44,16 @@ helm repo add elastic https://helm.elastic.co
 helm repo add stable https://kubernetes-charts.storage.googleapis.com
 helm repo update
 
+# install & update npm , install aws-cdk
+sudo npm install --global npm
+sudo npm install --global aws-cdk
+sudo npm install --global @aws-cdk/aws-ec2 @aws-cdk/aws-ecs @aws-cdk/aws-eks @aws-cdk/aws-iam @aws-cdk/aws-ecr
 
-
+# clean
 sudo apt autoremove --purge -y
 sudo apt autoclean
 sudo sync
+
+# install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
